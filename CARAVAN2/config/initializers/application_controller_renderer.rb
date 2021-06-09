6d9e3aeac15fb0c3c1a76fa18a022fe9a26b1312
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-06-09 05:58:05 +0000

    [update]

---
+++ CARAVAN2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '77a5ffb1b4d65b32693b09c086a16afd75131db1818c63cf31a812246d37dbd3200865b2b1a0bbb80fb8d70d14305e35ab200761753f563f9a4989b3ad724040'
